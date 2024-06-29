5b1a1cf6e0cf722e3ec895277c5e456854c39ef7
USER <aryandewangan948@gmail.com>
2024-06-29 13:20:50 +0530

    Final

---
+++ app/src/main/java/com/example/weatherforcast/splash_screen.kt
@@ -0,0 +1,24 @@
+package com.example.weatherforcast
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+
+class splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.statusBarColor=Color.GRAY
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
